18fd471492b2e4b8ee9e6990f5998a5c1240fc3d
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-14 03:35:50 +0000

    add index to email, add password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   # validates(:name, presence: true)
-  validates :email, presence: true, length: {maximum: 255}
+  
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]{1,}\.)+[a-z]+\z/i
+
+  validates :email, presence: true, length: {maximum: 255}, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_033423) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_032710) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
