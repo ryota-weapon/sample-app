0109ab51bde19bd553d89880bc11f9a2f661f4ce
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-14 08:53:12 +0000

    refactor layout file and integration test for controller

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test "invalid signup information" do
     get signup_path
 
     assert_no_difference "User.count" do
@@ -11,10 +11,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password_confirmation: "footbar"
       } } 
     end
-
     assert_response :unprocessable_entity
     assert_template :"users/new"
     assert_select "div#error_explanation"
     assert_select "div.alert"
+  end
+
+
+  test "valid signup information" do
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {name: "hoge",
+                                        email: "user@invali.feifefd",
+                                        password: "12345678",
+                                        password_confirmation: "12345678"
+      } }
+    end
+    follow_redirect!
+    assert_template :"users/show"
+    assert_not flash.empty?
   end
 end
