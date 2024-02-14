5ed799dfe5611372419dfaa7f305dec8e86470b4
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-14 03:53:15 +0000

    add validation for password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "foobar", password_confirmation: "foobar"
+                    password: "foobar333", password_confirmation: "foobar333"
     )
   end
 
@@ -62,6 +62,14 @@ class UserTest < ActiveSupport::TestCase
     assert_equal @user.reload.email, mixed_case_email.downcase
   end
 
-  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " "*6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a"*5
+    assert_not @user.valid?
+  end
 
 end
