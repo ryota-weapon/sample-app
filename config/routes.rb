ddd0f184fe100dd3806826802dae2f30dbad2f56
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-15 07:50:05 +0000

    make edit view and refactor signup page

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get  "/help", to: "static_pages#help"
   get  "/contact", to: "static_pages#contact"
   
+  # get "/edit", to: "users#edit"
   get "/signup", to: "users#new"
-
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: "logout"
